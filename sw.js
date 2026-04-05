0cdb1d4b7bc4c7e3a7dd18fbc95998b272814b4f
nvarma <nvarma@entainmobitech.com>
2026-04-05 18:16:29 +0530

    shrink title to single line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'museum-v21';
+const CACHE = 'museum-v22';
 const SHELL = ['./', './index.html', './css/styles.css', './js/api.js', './js/audio.js', './js/ui.js', './js/app.js'];
 
 self.addEventListener('install', e => {
