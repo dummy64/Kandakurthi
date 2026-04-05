c6468d7faf06e8e8a8788dc929ab24fa677a4048
nvarma <nvarma@entainmobitech.com>
2026-04-05 18:04:30 +0530

    increase detail page header, badge, desc, play button sizes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'museum-v15';
+const CACHE = 'museum-v16';
 const SHELL = ['./', './index.html', './css/styles.css', './js/api.js', './js/audio.js', './js/ui.js', './js/app.js'];
 
 self.addEventListener('install', e => {
